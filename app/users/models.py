aa7e7544981fff7a242d50b0edc45b58a994262f
jam <nevvjann@gmail.com>
2020-01-20 19:08:17 +0900

    Edit: finish user model add fields; language, currency choices add fields; birthdate, superhost

--- app/users/models.py
+++ app/users/models.py
@@ -8,17 +8,53 @@ class User(AbstractUser):
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
+    # 성별 선택
     GENDER_CHOICE = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other")
+        (GENDER_OTHER, "Other"),
     )
 
+    # 언어 선택
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_ENGLISH, "English"),
+    )
+
+    # 화폐 선택
+    CURRENCY_USD = "usd"
+    CURRENCY_WON = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_WON, "KRW"),
+        (CURRENCY_USD, "USD"),
+    )
+
+    # 프로필 이미지
     avatar = models.ImageField(null=True, blank=True)
+
     gender = models.CharField(
         choices=GENDER_CHOICE,
         max_length=10,
         null=True,
-        blank=True
+        blank=True,
     )
+    language = models.CharField(
+        choices=CURRENCY_CHOICES,
+        max_length=2,
+        null=True,
+        blank=True,
+    )
+    currency = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        null=True,
+        blank=True,
+    )
+
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    superhost = models.BooleanField(default=False)
