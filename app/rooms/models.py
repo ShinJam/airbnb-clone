302f8bfd8a3d6e525a04b54491cddaf481718154
jam <nevvjann@gmail.com>
2020-01-21 14:17:03 +0900

    Add: room type model

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -4,6 +4,21 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstracItem(core_models.TimeStampedModel):
+    """ Abstract Item """
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstracItem):
+    pass
+
+
 class Room(core_models.TimeStampedModel):
     """ Room Model  Definition """
 
@@ -21,3 +36,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_hook = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
