0b0668ae47683068fcf94bcb7fe8834cd0f24e9a
jam <nevvjann@gmail.com>
2020-01-21 15:10:15 +0900

    Edit: room model add room_type foreignkey add amenities manytomany add facilities manytomany house_rules = manaytomany

--- app/rooms/admin.py
+++ app/rooms/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
-class ItemAdmin(admin.ModelAdmin):
+@admin.register(models.Room)
+class RooAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(models.Room)
-class RooAdmin(admin.ModelAdmin):
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
     pass
--- app/rooms/models.py
+++ app/rooms/models.py
@@ -16,6 +16,22 @@ class AbstracItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstracItem):
+    """ RommType Model Definition """
+    pass
+
+
+class Amenity(AbstracItem):
+    """ Facility Model Definition """
+    pass
+
+
+class Facility(AbstracItem):
+    """ Facility Model Definition """
+    pass
+
+
+class HouseRule(AbstracItem):
+    """ HouseRule Model Definition """
     pass
 
 
@@ -36,7 +52,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_hook = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
 
     def __str__(self):
         return self.name
