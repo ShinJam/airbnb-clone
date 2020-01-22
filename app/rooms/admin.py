d57738f11c401d9ee0d380f01ca7e9cd2d11eb00
jam <nevvjann@gmail.com>
2020-01-22 11:24:57 +0900

    Add: Meta Class and Photos Model

--- app/rooms/admin.py
+++ app/rooms/admin.py
@@ -4,9 +4,11 @@ from . import models
 
 @admin.register(models.Room)
 class RooAdmin(admin.ModelAdmin):
+    """ Room Admin Definition """
     pass
 
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+    """ Item Admin Definition """
     pass
--- app/rooms/models.py
+++ app/rooms/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 
-class AbstracItem(core_models.TimeStampedModel):
+class AbstractItem(core_models.TimeStampedModel):
     """ Abstract Item """
     name = models.CharField(max_length=80)
 
@@ -15,24 +14,40 @@ class AbstracItem(core_models.TimeStampedModel):
         return self.name
 
 
-class RoomType(AbstracItem):
+class RoomType(AbstractItem):
     """ RommType Model Definition """
-    pass
+
+    class Meta:
+        verbose_name = "Room Type"
 
 
-class Amenity(AbstracItem):
+class Amenity(AbstractItem):
     """ Facility Model Definition """
-    pass
+
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
-class Facility(AbstracItem):
+class Facility(AbstractItem):
     """ Facility Model Definition """
-    pass
+
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
-class HouseRule(AbstracItem):
+class HouseRule(AbstractItem):
     """ HouseRule Model Definition """
-    pass
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
 
 
 class Room(core_models.TimeStampedModel):
@@ -51,11 +66,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_hook = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
