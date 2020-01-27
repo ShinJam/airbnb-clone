f75989f1cf939350e72817108f7b051c2514c464
jam <nevvjann@gmail.com>
2020-01-27 21:48:37 +0900

    Add: seed_lists

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -85,5 +85,5 @@ class Room(core_models.TimeStampedModel):
         if all_reviews:
             for review in all_reviews:
                 all_ratings += review.rating_average()
-            return all_ratings / len(all_reviews)
+            return round(all_ratings / len(all_reviews), 2)
         return 0
