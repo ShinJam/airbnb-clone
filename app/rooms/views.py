0c7e643ec015d389481a2c95d7323938b5f1b49f
jam <nevvjann@gmail.com>
2020-01-28 22:47:20 +0900

    Edit: Class Based Views

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,16 +1,12 @@
-from django.core.paginator import Paginator, EmptyPage
-from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        context = {
-            "page": paginator.page(page),
-        }
-        return render(request, "rooms/home.html", context)
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
