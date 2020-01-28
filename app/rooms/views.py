d0e43f85fec01d64b3d5e673202e1311373b93e6
jam <nevvjann@gmail.com>
2020-01-28 10:58:50 +0900

    Add: core template

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . import models
 
 
 def all_rooms(request):
-    return render(request, "all_rooms.html")
+    context = {
+        "potato": models.Room.objects.all(),
+    }
+    return render(request, "rooms/home.html", context)
