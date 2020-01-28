f13cb383f4ff2fd35477b0bffd6a5a2cc73b41c6
jam <nevvjann@gmail.com>
2020-01-28 16:28:44 +0900

    Edit: Handling Exception

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,26 +1,17 @@
-from math import ceil
-
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts import render, redirect
 from . import models
 
 
 def all_rooms(request):
-    # page = int(request.GET.get("page", 1))
-    # page_size = 10
-    # limit = page_size * page
-    # offset = limit - page_size
-    # page_count = ceil(models.Room.objects.count() / page_size)
-    # context = {
-    #     "potato": models.Room.objects.all()[offset:limit],
-    #     "page": page,
-    #     "page_count": page_count,
-    #     "page_range": range(1, page_count),
-    # }
     page = int(request.GET.get("page", 1))
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    context = {
-        "page": paginator.page(page),
-    }
-    return render(request, "rooms/home.html", context)
+    try:
+        rooms = paginator.page(page)
+        context = {
+            "page": paginator.page(page),
+        }
+        return render(request, "rooms/home.html", context)
+    except EmptyPage:
+        return redirect("/")
