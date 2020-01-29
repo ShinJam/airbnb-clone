74498d2038d420135491e2800c74a922e961e830
jam <nevvjann@gmail.com>
2020-01-29 21:27:35 +0900

    Edit: Using DetailView CBV

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.urls import reverse
 from django.http import Http404
 from . import models
@@ -15,11 +15,7 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        print(pk)
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        # return redirect(reverse("core:home"))
-        raise Http404()
+class RoomDetail(DetailView):
+    """ RoomDetail Definition """
+
+    model = models.Room
