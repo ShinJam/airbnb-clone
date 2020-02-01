000a705f0d3aeea72e65f49280e91bd104c0b7a3
jam <nevvjann@gmail.com>
2020-02-01 17:22:06 +0900

    Edit: Filtering Like a Boos 2

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from django_countries import countries
-from django.http import HttpResponse
-
 from . import models
 
 
 class HomeView(ListView):
+
     """ HomeView Definition """
 
     model = models.Room
@@ -17,42 +16,91 @@ class HomeView(ListView):
 
 
 class RoomDetail(DetailView):
+
     """ RoomDetail Definition """
 
     model = models.Room
 
 
 def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
     form = {
-        "city": request.GET.get("city", "Anywhere").capitalize(),
-        "s_room_type": int(request.GET.get("room_type", 0)),
-        "s_country": request.GET.get("country", "KR"),
-        "price": int(request.GET.get("price", 0)),
-        "guests": int(request.GET.get("guests", 0)),
-        "bedrooms": int(request.GET.get("bedrooms", 0)),
-        "beds": int(request.GET.get("beds", 0)),
-        "baths": int(request.GET.get("baths", 0)),
-        "s_amenities": request.GET.getlist("amenities"),
-        "s_facilities": request.GET.get("facilities"),
-        "instant": request.GET.get("instant", False),
-        "super_host": request.GET.get("super_host", False),
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "superhost": superhost,
     }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
 
     choices = {
         "countries": countries,
-        "room_types": models.RoomType.objects.all(),
-        "amenities": models.Amenity.objects.all(),
-        "facilities": models.Facility.objects.all(),
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
     }
+
     filter_args = {}
 
-    if form['city'] != "Anywhere":
-        filter_args["city__startswith"] = form['city']
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
 
-    filter_args["country"] = form['s_country']
+    filter_args["country"] = country
 
-    if form['s_room_type'] != 0:
-        filter_args["room_type__pk"] = form['s_room_type']
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
 
     rooms = models.Room.objects.filter(**filter_args)
 
