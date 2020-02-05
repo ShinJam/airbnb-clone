5edc91853cdfa54777ea4843086d932bfd903d35
jam <nevvjann@gmail.com>
2020-02-05 19:22:04 +0900

    Add: Login Form

--- app/users/views.py
+++ app/users/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from . import forms
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, "users/login.html")
+        form = forms.LoginForm()
+        print("this is form >>", form)
+        return render(request, "users/login.html", {'form': form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        print(form)
