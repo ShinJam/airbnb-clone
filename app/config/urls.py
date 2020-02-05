0159b8dc65fada98069b4ced55fd694e3cfbf5a4
jam <nevvjann@gmail.com>
2020-02-05 18:58:25 +0900

    Add: Login csv

--- app/config/urls.py
+++ app/config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("usrs/", include("users.urls", namespace="users")),
     path('admin/', admin.site.urls),
 ]
 
