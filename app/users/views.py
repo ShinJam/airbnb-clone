4484c1a26c64664f4882a98cdccbc00971075f0e
jam <nevvjann@gmail.com>
2020-02-09 18:49:56 +0900

    Add: Kakao login 3 - save avatar

--- app/users/views.py
+++ app/users/views.py
@@ -4,6 +4,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from . import forms, models
 
 
@@ -154,7 +155,6 @@ def kakao_callback(request):
             headers={"Authorization": f"Bearer {access_token}"},
         )
         profile_json = profile_request.json()
-        print(profile_json)
         email = profile_json.get("kaccount_email", None)
         if email is None:
             raise KakaoException()
@@ -175,6 +175,11 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                user.avatar.save(
+                    f"{nickname}-avatar", ContentFile(photo_request.content)
+                )
         login(request, user)
         return redirect("core:home")
     except KakaoException:
