7bab202c0d2bc5f5faebaef35bd32e0a5efdf6dc
jam <nevvjann@gmail.com>
2020-02-14 12:37:24 +0900

    Add: Room Photos

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.core.paginator import Paginator
 from django.views.generic import ListView, DetailView, View, UpdateView
 from django.shortcuts import render
+from users import mixins as user_mixins
 from . import models, forms
 
 
@@ -100,7 +102,7 @@ class SearchView(View):
         return render(request, "rooms/search.html", {"form": form})
 
 
-class EditRoomView(UpdateView):
+class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
     model = models.Room
     template_name = "rooms/room_edit.html"
     fields = (
@@ -122,3 +124,20 @@ class EditRoomView(UpdateView):
         "facilities",
         "house_rules",
     )
+
+    def get_object(self, queryset=None):
+        room = super().get_object(queryset=queryset)
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
+
+
+class RoomPhotosView(user_mixins.LoggedInOnlyView, DetailView):
+    model = models.Room
+    template_name = "rooms/room_photos.html"
+
+    def get_object(self, queryset=None):
+        room = super().get_object(queryset=queryset)
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
