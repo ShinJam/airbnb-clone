c1c1c95f7e082c6a50e347ad6f7ac3cea7596f39
jam <nevvjann@gmail.com>
2020-02-15 17:04:10 +0900

    Add: create BookedDays

--- app/reservations/admin.py
+++ app/reservations/admin.py
@@ -14,4 +14,9 @@ class ReservationAdmin(admin.ModelAdmin):
         "is_finished",
     )
 
-    list_filter = ("status", )
+    list_filter = ("status",)
+
+
+@admin.register(models.BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+    list_display = ("day", "reservation")
