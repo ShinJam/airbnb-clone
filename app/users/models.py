bae57c0b1a94209703b4589ba555b32a3914a911
jam <nevvjann@gmail.com>
2020-02-18 13:55:47 +0900

    Edit: Translating .py code

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -15,9 +16,9 @@ class User(AbstractUser):
 
     # 성별 선택
     GENDER_CHOICE = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other"),
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMALE, _("Female")),
+        (GENDER_OTHER, _("Other")),
     )
 
     # 언어 선택
@@ -25,8 +26,8 @@ class User(AbstractUser):
     LANGUAGE_KOREAN = "kr"
 
     LANGUAGE_CHOICES = (
-        (LANGUAGE_KOREAN, "Korean"),
-        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, _("Korean")),
+        (LANGUAGE_ENGLISH, _("English")),
     )
 
     # 화폐 선택
@@ -50,10 +51,18 @@ class User(AbstractUser):
 
     # 프로필 이미지
     avatar = models.ImageField(upload_to="avatars", blank=True)
-    gender = models.CharField(choices=GENDER_CHOICE, max_length=10, blank=True)
-    bio = models.TextField(blank=True)
+    gender = models.CharField(
+        _("gender"), choices=GENDER_CHOICE, max_length=10, blank=True
+    )
+    bio = models.TextField(_("bio"), blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=CURRENCY_CHOICES, max_length=2, blank=True)
+    language = models.CharField(
+        _("language"),
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+        default=LANGUAGE_KOREAN,
+    )
     currency = models.CharField(choices=LANGUAGE_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
@@ -73,7 +82,7 @@ class User(AbstractUser):
                 "emails/verify_email.html", {"secret": secret}
             )
             send_mail(
-                subject="Verify Airbnb Account",
+                subject=_("Verify Airbnb Account"),
                 message=None,
                 from_email=settings.EMAIL_FROM,
                 recipient_list=[self.email],
