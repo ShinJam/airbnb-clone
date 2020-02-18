0084871b3e8876f77efaf20baabf99b405d29e80
jam <nevvjann@gmail.com>
2020-02-18 16:22:34 +0900

    Add: go to Conversation

--- app/users/models.py
+++ app/users/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.core.mail import send_mail
 from django.shortcuts import reverse
 from django.template.loader import render_to_string
+from core import managers as core_managers
 
 
 class User(AbstractUser):
@@ -70,6 +71,7 @@ class User(AbstractUser):
     login_method = models.CharField(
         max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
+    objects = core_managers.CustomModelManager()
 
     def get_absolute_url(self):
         return reverse("users:profile", kwargs={"pk": self.pk})
