649430bc9a359e3476b34e5f8e0820e89d7fb3ef
jam <nevvjann@gmail.com>
2020-02-24 20:22:19 +0900

    Edit: CustomModelManager inherits UserManager To user createsuperuser

--- app/core/managers.py
+++ app/core/managers.py
@@ -1,7 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import UserManager
 
 
-class CustomModelManager(models.Manager):
+class CustomModelManager(UserManager):
     def get_or_none(self, **kwargs):
         try:
             return self.get(**kwargs)
